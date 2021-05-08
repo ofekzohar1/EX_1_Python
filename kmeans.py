4886ed8efa0529bbf7663cc8746ce86a99bc5c60
ben yeshurun <benyeshurun@mail.tau.ac.il>
2021-05-08 20:23:43 +0300

    need to be checked in nova

--- kmeans.py
+++ kmeans.py
@@ -1,5 +1,8 @@
-import math
 import sys
+
+default_iter = 200
+min_arguments = 2
+irrelevant_difference = 0.0001
 
 
 class Centroids:
@@ -42,7 +45,7 @@ def distance(vector_1, vector_2):
     distance_between_vectors = 0
     for number in range(len(vector_2)):
         distance_between_vectors += ((vector_1[number] - vector_2[number]) ** 2)
-    distance_between_vectors = math.sqrt(distance_between_vectors)
+    distance_between_vectors = distance_between_vectors
     return distance_between_vectors
 
 
@@ -59,11 +62,49 @@ def finding_cluster(list_of_centroids, vector):
     return distance_list.index(min(distance_list))
 
 
+# checks if new centrals had changed enough to keep iterating
+# return a boolean if we need to keep iterating
+def did_cluster_centroid_change(list_of_new_centrals, list_of_old_centrals):
+    changed = False
+    for ii in range(k):
+        for jj in range(dimensions):
+            if abs(list_of_new_centrals[ii][jj] - list_of_old_centrals[ii][jj]) > irrelevant_difference:
+                changed = True
+                break
+        if changed:
+            break
+    return changed
+
+
+# prints new central after adjusting for the relevant structure
+def print_centrals(a_central_list):
+    for central in a_central_list:
+        pointer = 0
+        for l in range(len(central)):
+            central[l] = "{:.4f}".format(central[l])
+        print(central)
+
+
+# Updates the new centroids and checks if we need to keep iterating
+# return TRUE if we need to keep iterating
+def update_centroids(a_centroids_list, k, the_new_central_list):
+    old_central_list = []
+    for j in range(k):
+        old_central_list.append(centroids_list[j].get_central())  # will be used for checking 0.0001 change
+        a_centroids_list[j].update_new_central()
+        the_new_central_list.append(centroids_list[j].central)
+        centroids_list[j].set_cluster_vectors([])
+    equal = did_cluster_centroid_change(the_new_central_list, old_central_list)
+    return equal
+
+
 if __name__ == '__main__':
+    if len(sys.argv) < min_arguments:
+        raise Exception(f"Amount of arguments should be more than 1, amount of arguments={len(sys.argv)}")
     if (not sys.argv[1].isnumeric()) or int(sys.argv[1]) < 1:
-        raise Exception(f"k input has to be a number and should exceed 0, k={sys.argv[1]}")
+        raise Exception(f"K input has to be a number and should exceed 0, k={sys.argv[1]}")
     k = int(sys.argv[1])
-    max_iter = 200
+    max_iter = default_iter
     if len(sys.argv) == 3:
         if (sys.argv[2].isnumeric()) and int(sys.argv[2]) > 1:
             max_iter = sys.argv[2]
@@ -85,6 +126,9 @@ if __name__ == '__main__':
             break
     # def main(k, max_iter=200, path=""):
     # list_of_vectors = move_data_to_array(path)
+    if k > len(list_of_vectors):
+        raise Exception(
+            f" K can't be bigger than the number of vectors, K={k},number of vectors ={len(list_of_vectors)}")
     centroids_list = []
     dimensions = len(list_of_vectors[0])
     for i in range(k):
@@ -93,30 +137,8 @@ if __name__ == '__main__':
         for vector in list_of_vectors:
             index = finding_cluster(centroids_list, vector)
             centroids_list[index].add_vector_to_cluster(vector)
-        old_central_list = []
         new_central_list = []
-        # resting the centroids group
-        for j in range(k):
-            old_central_list.append(centroids_list[j].get_central())  # will be used for checking 0.0004 change
-            centroids_list[j].update_new_central()
-            new_central_list.append(centroids_list[j].central)
-            centroids_list[j].set_cluster_vectors([])
-        # todo stoping with 0.004
-        count = 0
-        for old_central in old_central_list:
-            if distance(old_central, new_central_list[old_central_list.index(old_central)]) < 0.005:
-                count += 1
-        if count == k:
+        keep_iterating = update_centroids(centroids_list, k, new_central_list)
+        if not keep_iterating:
             break
-    for central in new_central_list:
-        pointer = 0
-        for l in central:
-            l = str(l)
-            point_index = l.find(".")
-            l = l[:point_index+5]
-            l = float(l)
-            central[pointer] = l
-            pointer += 1
-        print(central)
-
-#float("{0:.2f}".format(x))
+    print_centrals(new_central_list)
